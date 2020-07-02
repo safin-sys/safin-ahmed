f59a29a3fe35bd0da7cb486b8dc0da1341502bde
Safin Ahmed <61120114+safin-sys@users.noreply.github.com>
2020-07-02 23:24:15 +0600

    menu bar added

---
+++ main.js
@@ -0,0 +1,23 @@
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+    let getClose = document.querySelector('nav.phone');
+
+    if (toggleNavStatus === false) {
+        getClose.style.visibility = "visible";
+        toggleNavStatus = true;
+    }
+}
+
+let toggleNav2 = function() {
+    let getClose = document.querySelector('nav.phone');
+
+    if (toggleNavStatus === true) {
+        getClose.style.visibility = "hidden";
+        toggleNavStatus = false;
+    }
+}
+
+const comingSoon = function() {
+    alert('This feature is under construction, try again later.')
+}
