79031ef2f26407316f1f9d8a517da775f8847170
safin-sys <mdsafinofficial@gmail.com>
2021-12-08 00:08:46 +0600

    projects connected to github

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,58 +1,38 @@
 import { Image } from "@chakra-ui/image";
-import { Box, Flex, Grid, Heading, Link, Text } from "@chakra-ui/layout";
+import { Box, Flex, Grid, Heading, Link } from "@chakra-ui/layout";
 
-const images = [
-    {
-        src: "https://source.unsplash.com/random/800x600",
-        alt: "random image 1",
-    },
-    {
-        src: "https://source.unsplash.com/random/801x601",
-        alt: "random image 2",
-    },
-    {
-        src: "https://source.unsplash.com/random/802x602",
-        alt: "random image 3",
-    },
-    {
-        src: "https://source.unsplash.com/random/803x603",
-        alt: "random image 4",
-    },
-    {
-        src: "https://source.unsplash.com/random/804x604",
-        alt: "random image 5",
-    },
-    {
-        src: "https://source.unsplash.com/random/805x605",
-        alt: "random image 6",
-    }
-]
-
-export default function Projects() {
+export default function Projects({ projects }) {
     return (
         <Box py="10rem">
             <Heading color="secondary" fontWeight="normal" fontSize="12px" letterSpacing="3px" mb="30px">PROJECTS</Heading>
-            <Grid templateColumns={["1fr", "1fr 1fr"]} gap="1rem">
-                {images.map((image, index) => (
-                    <HoverOnImage key={index} {...image} />
+            <Grid templateColumns={["1fr", "1fr 1fr"]} gap="1.5rem 1rem">
+                {projects.map((project, index) => (
+                    <HoverOnImage key={index} {...project} />
                 ))}
             </Grid>
         </Box>
     )
 }
 
-const HoverOnImage = ({ src, alt }) => {
+const HoverOnImage = ({ name, homepage, html_url, default_branch }) => {
     return (
         <Box style={{ scrollSnapAlign: 'center' }}>
             <Image
-                src={src}
-                alt={alt}
+                src={`https://raw.githubusercontent.com/safin-sys/${name}/${default_branch}/logo.png`}
+                alt={name}
                 objectFit="cover"
                 objectPosition="center"
+                w="100%"
+                h="300px"
+                border="1px solid #a8a8a8"
             />
             <Flex fontSize="14px" letterSpacing="2px" justifyContent="space-between">
-                <Text>{alt}</Text>
-                <Link href="#">LIVE DEMO</Link>
+                <Link href={html_url} target="_blank" rel="noopener noreferrer">{name}</Link>
+                <Box>
+                    <Link href={html_url} target="_blank" rel="noopener noreferrer">github</Link>
+                    <span>/</span>
+                    <Link href={homepage} target="_blank" rel="noopener noreferrer">live demo</Link>
+                </Box>
             </Flex>
         </Box>
     )
--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,33 @@ import Hero from '../components/Hero'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
 
-export default function Home() {
+const featuredProjects = ['hear-the-unheard', 'cool-movie-app', 'awwa', 'rudra-nil-utsa', 'zapomnit-nato', 'seafood']
+
+function swapElement(array, indexA, indexB) {
+	const tmp = array[indexA];
+	array[indexA] = array[indexB];
+	array[indexB] = tmp;
+}
+
+export const getStaticProps = async () => {
+	const req = await fetch('https://api.github.com/users/safin-sys/repos')
+	const res = await req.json()
+	const projects = res.filter(({ name }) => featuredProjects.includes(name))
+	swapElement(projects, 2, 0)
+	swapElement(projects, 5, 4)
+	return {
+		props: {
+			projects
+		}
+	}
+}
+
+export default function Home({ projects }) {
 	return (
 		<Container maxW="container.lg" style={{ scrollSnapType: 'y mandatory' }}>
 			<Hero />
 			<Skills />
-			<Projects />
+			<Projects projects={projects} />
 			<Contact />
 		</Container>
 	)
