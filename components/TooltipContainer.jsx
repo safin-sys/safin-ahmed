f19c52214472b8f655e5e1ff9fe847f36fadaf1a
Safin Ahmed <mdsafinofficial@gmail.com>
2021-12-23 06:39:22 +0600

    4 items in stack now

--- components/TooltipContainer.jsx
+++ components/TooltipContainer.jsx
@@ -3,7 +3,7 @@ import { Container, Tooltip } from "@chakra-ui/react";
 export const TooltipContainer = ({ children, label }) => {
     return (
         <Tooltip label={label}>
-            <Container centerContent flex="0 0 calc(24% - 3rem)">
+            <Container centerContent flex="0 0 calc(25% - 3rem)">
                 {children}
             </Container>
         </Tooltip>
