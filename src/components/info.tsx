08256a2eea589dadaa33e8a09a556010ee7a7408
safin-calling <safinahmed248@gmail.com>
2025-05-17 00:15:23 +0600

    refactor info: changed font weight, added elipsis onn overflow

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -22,9 +22,9 @@ const Info = () => {
     return (
         <ul className="container p-4 flex flex-col gap-2 text-[14px]">
             {infos.map((info, key) => (
-                <li key={key} className="flex items-center gap-1 font-normal">
+                <li key={key} className="flex items-center gap-1.5 font-light">
                     <span className="border p-0.5 rounded-md">{info.icon}</span>
-                    {info.text}
+                    <span className="truncate">{info.text}</span>
                 </li>
             ))}
         </ul>
