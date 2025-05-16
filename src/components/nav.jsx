2219e0d8cdc70dff42c47ffccd86104dc9e82a4b
safin-calling <safinahmed248@gmail.com>
2025-05-16 21:27:38 +0600

    swoosh animation added

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,29 +1,29 @@
 "use client";
 
 import { useState } from "react";
+import Swoosh from "./icons/swoosh";
 
 const Nav = () => {
     const links = ["Home", "Experience", "Projects", "Blog"];
     const [activeLink, setActiveLink] = useState("Home");
+    const [hoveredLink, setHoveredLink] = useState(null);
     return (
         <nav className="container flex items-center h-16 font-heading font-normal justify-between px-2">
             {/* <img src="logo.svg" alt="Safin Ahmed Logo" /> */}
             <div className="links flex gap-4">
                 {links.map((link, key) => (
                     <a
-                        className="flex flex-col items-center relative group"
+                        className="flex flex-col items-center relative"
                         href="#"
                         key={key}
+                        onMouseEnter={() => setHoveredLink(link)}
+                        onMouseLeave={() => setHoveredLink(null)}
                     >
                         {link}
-                        <img
-                            className={`${
-                                activeLink === link
-                                    ? ""
-                                    : "invisible group-hover:visible"
-                            } absolute bottom-0`}
-                            src="swoosh.svg"
-                            alt=""
+                        <Swoosh
+                            animate={
+                                activeLink === link || hoveredLink === link
+                            }
                         />
                     </a>
                 ))}
