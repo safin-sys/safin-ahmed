ceff050ee488969fdbf77b5df886b56e1d6b942a
safin-sys <mdsafinofficial@gmail.com>
2025-05-17 11:19:43 +0600

    fix: pattern divider container borders added

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -1,21 +1,23 @@
 const Divider = ({
-    orientation = "horizontal",
-    pattern = false,
+  orientation = "horizontal",
+  pattern = false,
 }: {
-    orientation?: "vertical" | "horizontal";
-    pattern?: boolean;
+  orientation?: "vertical" | "horizontal";
+  pattern?: boolean;
 }) => {
-    if (pattern)
-        return (
-            <div className="flex h-8 w-full bg-[repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] border-y border-border" />
-        );
+  if (pattern)
     return (
-        <div
-            className={`${
-                orientation === "vertical" ? "h-full w-px" : "w-full h-px"
-            } bg-border`}
-        />
+      <div className="flex h-8 w-full bg-[repeating-linear-gradient(315deg,var(--border)_0,var(--border)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] border-y border-border">
+        <div className="container" />
+      </div>
     );
+  return (
+    <div
+      className={`${
+        orientation === "vertical" ? "h-full w-px" : "w-full h-px"
+      } bg-border`}
+    />
+  );
 };
 
 export default Divider;
