3d0c83df7063d8a976a009b9c4c9b4ba941c6abc
safin-calling <safinahmed248@gmail.com>
2025-06-05 12:22:44 +0600

    kill switch activated

--- src/app/api/sd/route.ts
+++ src/app/api/sd/route.ts
@@ -1,5 +1,5 @@
 export const GET = () => {
-  return new Response("self-destruct-sta", {
+  return new Response("self-destruct", {
     status: 200,
     headers: {
       "Content-Type": "text/plain"
