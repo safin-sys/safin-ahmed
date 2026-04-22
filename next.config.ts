71e88ea2198e4b96d9c9f8c4f81f88ffb0dcb16d
Safin Ahmed <mdsafinofficial@gmail.com>
2026-04-22 23:30:00 +0600

    Update next.config.ts profile picture url

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     /* config options here */
     images: {
         remotePatterns: [
-            new URL("https://avatars.githubusercontent.com/u/61120114?v=4"),
+            new URL("https://github.com/safin-sys.png"),
             new URL("https://raw.githubusercontent.com/safin-sys/*/*/logo.png"),
         ],
     },
